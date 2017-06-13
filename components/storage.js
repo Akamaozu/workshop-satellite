26be5aefaf4e9a11955d30035e28f7c1705ad390
Uzo Olisemeka <uzo@designbymobius.ca>
2017-06-13 11:56:35 -0400

    standardized camera mail methods and responses

--- components/storage.js
+++ components/storage.js
@@ -13,13 +13,46 @@ var citizen = require('supe'),
 
         var filename = mail.name,
             content = mail.content,
-            type = mail.content_type || 'utf8';
+            type = mail.content_type || 'utf8',
+
+            sender = envelope.from;
 
         fs_extra.outputFile( path.join( 'storage', filename ), content, type, function( err ){
 
           if( err ) throw err;
 
-          console.log( 'saved ' + filename );
+          citizen.noticeboard.notify( 'file-saved', { name: filename, requester: sender });
+          console.log( 'action=save file=' + filename + ' requester=' + sender );
+
+          ack();
+        });
+
+      break;
+
+      case 'get-file':
+
+      var filename = mail.name;
+
+        if( !filename ){
+
+          console.log( 'cannot retrieve file if name isn\'t specified', envelope );
+          ack();
+        }
+
+        fs_extra.readFile( path.join( 'storage', filename ), function( err, content ){
+
+          var response = { name: filename, success: false };
+
+          if( err ) response.error = error.toString();
+
+          else {
+
+            response.success = true;
+            response.content = content;
+          }
+
+          citizen.noticeboard.notify( 'get-file-result', response );
+          console.log( 'action=get file=' + filename + ' requester=' + sender );
           ack();
         });
 
@@ -29,18 +62,20 @@ var citizen = require('supe'),
 
         fs_extra.readdir( 'storage', function( err, files ){
 
-          if( err ) throw err;
+          var response = { success: false };
 
-          citizen.noticeboard.notify( 'stored-files', files );
+          if( !err ) response.files = files;
+
+          citizen.noticeboard.notify( 'list-stored-files-result', response );
+          console.log( 'action=list requester=' + sender );
           ack();
         });
 
       break;
 
       default:
-        console.log( 'possibly malformed mail', envelope );
+        console.log( 'cannot properly process this mail: ', envelope );
         ack();
       break;
-
     }
   });
